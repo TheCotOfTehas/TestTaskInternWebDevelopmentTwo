3304605d9ae286523642bd46a311fd16b4fb7695
THECOTOFTEHAS\tsebr <tsebroevgeny@yandex.ru>
2022-12-05 18:50:17 +0300

    init

---
+++ Domain/PointWeb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PointWebDomain
+{
+    internal class PointWeb
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public double Radius { get; set; }
+        public string Color { get; set; }
+        public List<Сomment> Comments { get; set; }
+    }
+}
---
+++ Domain/Сomment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointWebDomain
+{
+    internal class Сomment
+    {
+        public string BackgroundСolor { get; set; }
+        public string Text { get; set; }
+    }
+}
